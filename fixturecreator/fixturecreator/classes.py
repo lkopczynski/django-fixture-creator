e5c4bab5f40b6d5ebcf4efabfc09f9aaa7f3ec4d
Lorenz Kopczynski <Lorenz.Kopczynski@digitecgalaxus.ch>
2019-07-30 15:20:52 +0200

    seconds commit

--- fixturecreator/fixturecreator/classes.py
+++ fixturecreator/fixturecreator/classes.py
@@ -1,17 +1,34 @@
 import json
+import random
 
 
 class FixtureCreator:
 
-    pattern = None
+    generators = [
+        ('string', 'RandomStringGenerator')
+    ]
+
+    _pattern = None
+    _elements = []
 
     def __init__(self):
         pass
 
-    def loadPattern(self, json_string):
-        self.pattern = json.loads(json)
-
+    def load_pattern(self, json_string):
+        self._pattern = json.loads(json_string)
 
     def go_over_pattern(self):
-        if pattern is None:
+        if self._pattern is None:
             raise ValueError('No pattern defined!')
+
+        for element in self._pattern:
+            self._create_element(element)
+
+    def _create_element(self, data):
+        element = {
+            "model": data['model'],
+            "pk": random.randint(1, 100000)
+        }
+
+        for field in data['fields']:
+            print(field['type'])
--- test/test_generators.py
+++ test/test_generators.py
@@ -1,6 +1,6 @@
 import unittest
 
-from fixturecreator.generators import RandomSequenceGenerator
+from fixturecreator.fixturecreator.generators import RandomSequenceGenerator
 
 class TestRandomSequenceGenerator(unittest.TestCase):
 
@@ -13,6 +13,10 @@ class TestRandomSequenceGenerator(unittest.TestCase):
         self.assertTrue(len(value) < 9)
         self.assertTrue(len(value) > 4)
 
-    def test_exact_legth(self):
-        self.rsg = RandomSequenceGenerator(exact_length=8)
+    def test_exact_length(self):
+        self.rsg = RandomSequenceGenerator(exact_length=8, characters='a')
         self.assertEqual(len(self.rsg.value), 8)
+
+    def test_characters(self):
+        self.rsg = RandomSequenceGenerator(exact_length=5, characters='a')
+        self.assertEqual(self.rsg.value, 'aaaaa')
